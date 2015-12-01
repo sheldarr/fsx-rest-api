d3cb5e76d9808652b08f3817ed27775e79554ad2
sheldar@wp.pl <sheldar@wp.pl>
2015-12-01 16:54:52 +0100

    Add base Nancy solution

---
+++ FsxApi/FsxApi/FsxApi.cs
@@ -0,0 +1,21 @@
+﻿namespace FsxApi
+{
+    using Nancy;
+
+    class FsxApi : NancyModule
+    {
+        public FsxApi() : base("/api/fsx")
+        {
+            Get["/"] = _ =>
+            {
+                var response = new
+                {
+                    Code = 200,
+                    Message = "OK"
+                };
+
+                return Response.AsJson(response);
+            };
+        }
+    }
+}
---
+++ FsxApi/FsxApi/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FsxApi
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseUri = new Uri("http://localhost:7070");
+
+            var hostConfiguration = new HostConfiguration
+            {
+                RewriteLocalhost = true,
+                UrlReservations = new UrlReservations
+                {
+                    CreateAutomatically = true
+                }
+            };
+
+            using (var host = new NancyHost(hostConfiguration, baseUri))
+            {
+                host.Start();
+
+                Console.WriteLine("Application started at {0}:{1}", baseUri.Host, baseUri.Port);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
