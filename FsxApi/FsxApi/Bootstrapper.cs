4d32e89ce79f1e276b56369ce66ee8c11905d6e4
sheldar@wp.pl <sheldar@wp.pl>
2015-12-08 20:51:52 +0100

    Add Access-Control-Allow-Origin to reach response

--- FsxApi/FsxApi/Bootstrapper.cs
+++ FsxApi/FsxApi/Bootstrapper.cs
@@ -2,6 +2,7 @@
 {
     using Fsx;
     using Nancy;
+    using Nancy.Bootstrapper;
     using Nancy.TinyIoc;
 
     public class Bootstrapper : DefaultNancyBootstrapper
@@ -11,6 +12,14 @@
             base.ConfigureApplicationContainer(container);
 
             container.Register<FsxDataRepository>().AsSingleton();
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            pipelines.AfterRequest += (context) =>
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            };
         }
     }
 }
